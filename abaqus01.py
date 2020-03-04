ca718b8a0a0e4a369a8a9f691623d806d4b852cd
crankyyash <61792333+crankyyash@users.noreply.github.com>
2020-03-04 20:12:13 +0530

    Add files via upload

---
+++ abaqus01.py
@@ -0,0 +1,25 @@
+x=1; #Element number
+theta=0; #material orientation angle
+t=0.000104; #thickness
+i=5; #number of integration points
+c=1; #compositelayup number
+def generate():
+        seq=(str(theta),".");
+        mod_theta="".join(seq);
+        print("*Elset, elset=CompositeLayup-",c,"-1, generate",file=myfile);
+        print("   ",x," ,",x+6630,", 170",file=myfile);
+        print("** Section: CompositeLayup-",c,"-1",file=myfile);
+        print("*Shell Section,elset=CompositeLayup-",c,"-1, composite, layup=CompositeLayup-",c,file=myfile);
+        print("",t,", ",i,", cylinder, ",mod_theta,", ply-1",file=myfile);
+while x<171:
+        myfile=open('composite.txt','a');
+        if x==170:
+                theta=90;
+                generate();
+                myfile.close();
+                break;
+        generate();
+        x=x+1;
+        theta=round(theta+float(90/170),2);
+        c=c+1;
+        myfile.close();
