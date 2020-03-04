b70933e3ce5d19f00f78fddc274a135b644e7914
crankyyash <61792333+crankyyash@users.noreply.github.com>
2020-03-04 21:18:36 +0530

    Update abaqus01.py

--- abaqus01.py
+++ abaqus01.py
@@ -5,14 +5,12 @@ i=5; #number of integration points
 c=1; #compositelayup number
 k=6630; #difference between your topmost and bottomost element number
 def generate():
-        seq=(str(theta),".");
-        mod_theta="".join(seq);
         print("** Region: (CompositeLayup-",c,"-1: Generated From Layup)",sep="",file=myfile)
         print("*Elset, elset=CompositeLayup-",c,"-1, generate",sep="",file=myfile);
         print("   ",x,",  ",x+k,",   170",sep="",file=myfile);
         print("** Section: CompositeLayup-",c,"-1",sep="",file=myfile);
         print("*Shell Section,elset=CompositeLayup-",c,"-1, composite, layup=CompositeLayup-",c,sep="",file=myfile);
-        print("",t,", ",i,", cylinder, ",mod_theta,", Ply-1",sep="",file=myfile);
+        print("",t,", ",i,", cylinder, ",theta,", Ply-1",sep="",file=myfile);
 while x<171:
         myfile=open('composite.txt','a');
         if x==170:
